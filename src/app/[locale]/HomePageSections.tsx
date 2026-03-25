ced1434af848216ff4ff07027f603517d1202f73
Akihiro Nishigai <akisky2967@gmail.com>
2026-03-25 21:57:28 +0900

    List view: restore 16:9 thumbnail with self-start, text flows right

--- src/app/[locale]/HomePageSections.tsx
+++ src/app/[locale]/HomePageSections.tsx
@@ -951,7 +951,7 @@ function ProjectsSection({ initialProjects }: { initialProjects: CompletedProjec
                   <div
                     className={`bg-muted overflow-hidden relative ${effectiveViewMode === 'grid'
                       ? 'aspect-video rounded-t-xl'
-                      : 'w-28 min-w-28 sm:w-52 sm:min-w-52 rounded-l-xl rounded-tr-none shrink-0'
+                      : 'w-40 min-w-40 sm:w-56 sm:min-w-56 aspect-video self-start rounded-l-xl rounded-tr-none shrink-0'
                       }`}
                   >
                     {project.image ? (
