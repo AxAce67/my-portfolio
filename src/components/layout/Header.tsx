7f44644f635061d434f3e2f515e34d47c3f0377a
Akhiro Nishigai <akisky2967@gmail.com>
2026-08-09 18:35:56 +0900

    Stabilize Safari fixed header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -177,7 +177,7 @@ export function Header() {
     return (
         <>
             <header
-                className={`site-header sticky left-0 right-0 z-50 ${shouldShowSolidHeader ? 'is-scrolled' : ''}`}
+                className={`site-header fixed left-0 right-0 z-50 ${shouldShowSolidHeader ? 'is-scrolled' : ''}`}
             >
                 <nav className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-14 sm:h-16">
